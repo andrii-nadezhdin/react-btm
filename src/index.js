692295f9b655cbeee8fcf8777f15bb56d2dba9de
Andrii Nadezhdin <andrii.nadezhdin@betssongroup.com>
2019-08-08 13:13:08 +0300

    add debug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 
 import ReactDOM from 'react-dom';
 import React, { setGlobal } from 'reactn';
+import addReactNDevTools from 'reactn-devtools';
 import App from './components/app';
+
+addReactNDevTools();
 
 setGlobal({
     todos: [],
